4165cf0347dec000520da4440dd1e9cb6835f39f
andrsgrza <andresgarza1998@gmail.com>
2023-11-29 07:30:45 -0600

    First commit

---
+++ src/main/java/com/angaar/odysseia/OdysseiaApplication.java
@@ -0,0 +1,13 @@
+package com.angaar.odysseia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdysseiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdysseiaApplication.class, args);
+	}
+
+}
